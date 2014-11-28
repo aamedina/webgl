febde1cf125f34150a91b7a33c67292f94bdda4a
Adrian Medina <adrian.medina@mail.yu.edu>
2014-11-27 20:32:07 -0500

    add helper macros

--- src/cljs/webgl/core.clj
+++ src/cljs/webgl/core.clj
@@ -1,5 +1,5 @@
 (ns webgl.core
-  (:refer-clojure :exclude [compile])
+  (:refer-clojure :exclude [compile flush])
   (:require [clojure-gl.compiler :refer [compile-ns]]            
             [clojure.tools.logging :as log]
             [cljs.analyzer :as ana]))
@@ -17,3 +17,322 @@
   `(THREE.ShaderMaterial. (doto ~opts
                             (aset "vertexShader" (compile ~vertex))
                             (aset "fragmentShader" (compile ~fragment)))))
+
+(defmacro context-attributes
+  []
+  `(.getContextAttributes *gl*))
+
+(defmacro context-lost?
+  []
+  `(.isContextLost *gl*))
+
+(defmacro supported-extensions
+  []
+  `(.getSupportedExtensions *gl*))
+
+(defmacro extension
+  [name]
+  `(.getExtension *gl* ~name))
+
+(defmacro active-texture
+  [texture]
+  `(.activeTexture *gl* ~texture))
+
+(defmacro attach-shader
+  [program shader]
+  `(.attachShader *gl* ~program ~shader))
+
+(defmacro bind-attrib-location
+  [program index name]
+  `(.bindAttribLocation *gl* ~program ~index ~name))
+
+(defmacro bind-buffer
+  [target buffer]
+  `(.bindBuffer *gl* ~target ~buffer))
+
+(defmacro bind-framebuffer
+  [target buffer]
+  `(.bindFramebuffer *gl* ~target ~buffer))
+
+(defmacro bind-renderbuffer
+  [target buffer]
+  `(.bindRenderbuffer *gl* ~target ~buffer))
+
+(defmacro bind-texture
+  [target texture]
+  `(.bindTexture *gl* ~target ~texture))
+
+(defmacro blend-color
+  [r g b a]
+  `(.blendColor *gl* ~r ~g ~b ~a))
+
+(defmacro blend-equation
+  [mode]
+  `(.blendEquation *gl* ~mode))
+
+(defmacro blend-equation-separate
+  [mode-rgb mode-alpha]
+  `(.blendEquationSeparate *gl* ~mode-rgb ~mode-alpha))
+
+(defmacro blend-func
+  [sfactor dfactor]
+  `(.blendFunc *gl* ~sfactor ~dfactor))
+
+(defmacro blend-func-separate
+  [src-rgb dst-rgb src-alpha dst-alpha]
+  `(.blendFuncSeparate *gl* ~src-rgb ~dst-rgb ~src-alpha ~dst-alpha))
+
+(defmacro buffer-data
+  [target data usage]
+  `(.bufferData *gl* ~target ~data ~usage))
+
+(defmacro buffer-sub-data
+  [target offset data]
+  `(.bufferSubData *gl* ~target ~offset ~data))
+
+(defmacro check-framebuffer-status
+  [target]
+  `(.checkFramebufferStatus *gl* ~target))
+
+(defmacro clear
+  [mask]
+  `(.clear *gl* ~mask))
+
+(defmacro clear-color
+  [r g b a]
+  `(.clearColor *gl* ~r ~g ~b ~a))
+
+(defmacro clear-depth
+  [depth]
+  `(.clearDepth *gl* ~depth))
+
+(defmacro clear-stencil
+  [s]
+  `(.clearStencil *gl* ~s))
+
+(defmacro color-mask
+  [r g b a]
+  `(.colorMask *gl* ~r ~g ~b ~a))
+
+(defmacro compressed-tex-image-2d
+  [target level internalformat width height border data]
+  `(.compressedTexImage2D *gl* ~target ~level ~internalformat ~width ~height
+                          ~border ~data))
+
+(defmacro compressed-tex-sub-image-2d
+  [target level xoffset yoffset width height border]
+  `(.compressedTexSubImage2D *gl* ~target ~level ~xoffset ~yoffset
+                             ~width ~height ~border))
+
+
+(defmacro copy-tex-image-2d
+  [target level internalformat x y width height border]
+  `(.copyTexImage2D *gl* ~target ~level ~internalformat ~x ~y ~width ~height
+                    ~border))
+
+(defmacro copy-tex-sub-image-2d
+  [target level xoffset yoffset x y width height border]
+  `(.copyTexSubImage2D *gl* ~target ~level ~xoffset ~yoffset ~x ~y ~width
+                       ~height))
+
+(defmacro create-buffer
+  []
+  `(.createBuffer *gl*))
+
+(defmacro create-framebuffer
+  []
+  `(.createFramebuffer *gl*))
+
+(defmacro create-program
+  []
+  `(.createProgram *gl*))
+
+(defmacro create-renderbuffer
+  []
+  `(.createRenderbuff *gl*))
+
+(defmacro create-shader
+  [type]
+  `(.createShader *gl* ~type))
+
+(defmacro create-texture
+  []
+  `(.createTexture *gl*))
+
+(defmacro cull-face
+  [mode]
+  `(.cullFace *gl* ~mode))
+
+(defmacro delete-buffer
+  [buffer]
+  `(.deleteBuffer *gl* ~buffer))
+
+(defmacro delete-framebuffer
+  [framebuffer]
+  `(.deleteFramebuffer *gl* ~framebuffer))
+
+(defmacro delete-program
+  [program]
+  `(.deleteProgram *gl* ~program))
+
+(defmacro delete-renderbuffer
+  [renderbuffer]
+  `(.deleteRenderbuffer *gl* ~renderbuffer))
+
+(defmacro delete-shader
+  [shader]
+  `(.deleteShader *gl* ~shader))
+
+(defmacro delete-shader
+  [texture]
+  `(.deleteTexture *gl* ~texture))
+
+(defmacro depth-func
+  [func]
+  `(.depthFunc *gl* ~func))
+
+(defmacro depth-mask
+  [flag]
+  `(.depthMask *gl* ~flag))
+
+(defmacro depth-range
+  [z-near z-far]
+  `(.depthRange *gl* ~z-near ~z-far))
+
+(defmacro detach-shader
+  [program shader]
+  `(.detachShader *gl* ~program ~shader))
+
+(defmacro disable
+  [cap]
+  `(.disable *gl* ~cap))
+
+(defmacro disable-vertex-attrib-array
+  [index]
+  `(.disableVertexAttribArray *gl* ~index))
+
+(defmacro draw-arrays
+  [mode first count]
+  `(.drawArrays *gl* ~mode ~first ~count))
+
+(defmacro draw-elements
+  [mode count type offset]
+  `(.drawElements *gl* ~mode ~count ~type ~offset))
+
+(defmacro enable
+  [cap]
+  `(.enable *gl* ~cap))
+
+(defmacro enable-vertex-attrib-array
+  [index]
+  `(.enableVertexAttribArray *gl* ~index))
+
+(defmacro finish
+  []
+  `(.finish *gl*))
+
+(defmacro flush
+  []
+  `(.flush *gl*))
+
+(defmacro framebuffer-renderbuffer
+  [target attachment renderbuffertarget renderbuffer]
+  `(.framebufferRenderbuffer *gl* ~target ~attachment ~renderbuffertarget
+                             ~renderbuffer))
+
+(defmacro framebuffer-texture-2d
+  [target attachment textarget texture level]
+  `(.framebufferTexture2D *gl* ~target ~attachment ~textarget ~texture ~level))
+
+(defmacro front-face
+  [mode]
+  `(.frontFace *gl* ~mode))
+
+(defmacro generate-mipmap
+  [target]
+  `(.generateMipmap *gl* ~target))
+
+(defmacro active-attrib
+  [program index]
+  `(.getActiveAttrib *gl* ~program ~index))
+
+(defmacro active-uniform
+  [program index]
+  `(.getActiveUniform *gl* ~program ~index))
+
+(defmacro attached-shaders
+  [program]
+  `(.getAttachedShaders *gl* ~program))
+
+(defmacro attrib-location
+  [program name]
+  `(.getAttribLocation *gl* ~program ~name))
+
+(defmacro buffer-parameter
+  [target pname]
+  `(.getBufferParameter *gl* ~target ~pname))
+
+(defmacro parameter
+  [pname]
+  `(.getParameter *gl* ~pname))
+
+(defmacro error
+  []
+  `(.getError *gl*))
+
+(defmacro use-program
+  [program]
+  `(.useProgram *gl* ~program))
+
+(defmacro link-program
+  [program]
+  `(.linkProgram *gl* ~program))
+
+(defmacro program-parameter
+  [program pname]
+  `(.getProgramParameter *gl* ~program ~pname))
+
+(defmacro shader-parameter
+  [shader pname]
+  `(.getShaderParameter *gl* ~shader ~pname))
+
+(defmacro shader-source
+  [shader source]
+  `(.shaderSource *gl* ~shader ~source))
+
+(defmacro compile-shader
+  [shader]
+  `(let [shader# ~shader
+         _# (.compileShader *gl* shader#)
+         status# (shader-parameter shader# gl/COMPILE_STATUS)]
+         (if (pos? status#)
+           shader#
+           (do (js/console.log (.getShaderInfoLog *gl* shader#))
+               (delete-shader shader#)))))
+
+(defmacro program
+  [vertex fragment]
+  `(let [vertex# (doto (create-shader goog.webgl/VERTEX_SHADER)
+                   (shader-source (compile ~vertex))
+                   (compile-shader))
+         fragment# (doto (create-shader goog.webgl/FRAGMENT_SHADER)
+                     (shader-source (compile ~fragment))
+                     (compile-shader))
+         program# (create-program)]
+     (doto program#
+       (attach-shader vertex#)
+       (attach-shader fragment#)
+       (link-program))     
+     (let [link-status# (program-parameter program# goog.webgl/LINK_STATUS)]
+       (if (pos? link-status#)
+         program#
+         (do (js/console.log (.getProgramInfoLog program#))
+             (delete-program program#)
+             (delete-shader fragment#)
+             (delete-shader vertex#))))))
+
+(defmacro init-shaders
+  [vertex fragment]
+  `(let [program# (program ~vertex ~fragment)]
+     (set! (.-program *gl*) program#)
+     (use-program program#)))
--- src/cljs/webgl/core.cljs
+++ src/cljs/webgl/core.cljs
@@ -1,8 +1,56 @@
 (ns webgl.core
-  (:require [dommy.core :refer-macros [sel1]])
-  (:require-macros [webgl.core :refer [shader-material]]))
+  (:require [om.core :as om]
+            [om.dom :as dom]
+            [dommy.core :refer-macros [sel1]]
+            [goog.events :as e]
+            [goog.events.EventType :as et]
+            [goog.webgl :as gl])
+  (:require-macros [webgl.core :as gl]))
+
+(defonce app-state (atom {}))
+
+(def ^:dynamic *gl* nil)
+
+(def frame 0)
+
+(defn canvas
+  [{:keys [setup render] :as data} owner]
+  (reify
+    Object
+    (onResize [_ e]
+      (js/console.log e))
+    om/IDidMount
+    (did-mount [this]
+      (let [canvas (om/get-node owner)] 
+        (binding [*gl* (.getContext canvas "webgl")]
+          (setup data owner)
+          (letfn [(render-frame []
+                    (set! frame (js/requestAnimationFrame render-frame))
+                    (render data owner))]
+            (e/listen js/window et/RESIZE (.-onResize this) false this)
+            (render-frame)))))
+    om/IWillUnmount
+    (will-unmount [this]
+      (js/cancelAnimationFrame frame)
+      (e/unlisten js/window et/RESIZE (.-onResize this) false this))
+    om/IShouldUpdate
+    (should-update [_ _ _] false)
+    om/IRender
+    (render [_]
+      (dom/canvas #js {:id "canvas"
+                       :width js/window.innerWidth
+                       :height js/window.innerHeight}))))
+
+(defn setup
+  [data owner]
+  (gl/clear-color 0.0 0.0 0.0 1.0)
+  (gl/clear gl/COLOR_BUFFER_BIT))
+
+(defn render
+  [data owner]
+  )
 
 (defn -main
   []
-  (let [canvas (sel1 :#app)]
-    (js/console.log canvas)))
+  (swap! app-state assoc :render render :setup setup)
+  (om/root canvas app-state {:target (sel1 :#app)}))
